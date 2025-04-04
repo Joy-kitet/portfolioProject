eafdc484c7bad93a69cbaa98790f26113900921f
Joy-kitet <debrajoykitet@gmail.com>
2025-04-04 22:26:41 +0300

    modified files

--- index.js
+++
@@ -1,5 +0,0 @@
-// Toggle the menu when the hamburger icon is clicked
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
