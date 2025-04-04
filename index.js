8cf8c0893e49d368e7d6850c52d4be9d028a33e0
Joy-kitet <debrajoykitet@gmail.com>
2025-04-04 22:22:30 +0300

    Portfolio website

---
+++ index.js
@@ -0,0 +1,5 @@
+// Toggle the menu when the hamburger icon is clicked
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
